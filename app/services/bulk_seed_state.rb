4fc48477d34e69361c859f95d8a1c469bea0a452
kamalasaurus <kamalasaurus@gmail.com>
2022-08-23 12:51:30 -0400

    fixture cleanup, test scaffold

---
+++ app/services/bulk_seed_state.rb
@@ -0,0 +1,10 @@
+module BulkSeedState
+  def self.upload(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      # parse bin/seed stuff from csv here
+      # if seedbox doesn't exist, create
+      # if seed doesn't exist create
+      # create bin pointing to seedbox and seed
+    end
+  end
+end
---
+++ test/services/bulk_seed_state_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class BulkSeedStateTest < ActiveSupport::TestCase
+
+  setup do
+  end
+
+  test 'it should upload csv to db' do
+    assert true
+  end
+end
