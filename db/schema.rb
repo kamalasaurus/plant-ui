abe92ca7cca9e2b4abc08a3be020445ff48535b0
kamalasaurus <kamalasaurus@gmail.com>
2022-10-05 15:50:50 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_03_191218) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_05_193351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -198,11 +198,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_03_191218) do
   end
 
   create_table "populations", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "population_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "subpopulation", null: false
-    t.index ["name", "subpopulation"], name: "index_populations_on_name_and_subpopulation", unique: true
+    t.index ["population_name", "subpopulation"], name: "index_populations_on_population_name_and_subpopulation", unique: true
   end
 
   create_table "root_communities", force: :cascade do |t|
