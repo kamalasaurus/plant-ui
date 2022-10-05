201dbff9f9a548129d3c2326a32fdf1c5103b4d9
kamalasaurus <kamalasaurus@gmail.com>
2022-10-05 17:12:19 -0400

    migration cleanup

--- db/migrate/20221005202744_add_accession_reference_to_seeds_and_plant_samples.rb
+++ db/migrate/20221005202744_add_accession_reference_to_seeds_and_plant_samples.rb
@@ -1,11 +1,12 @@
 class AddAccessionReferenceToSeedsAndPlantSamples < ActiveRecord::Migration[7.0]
   def change
-    add_reference :seeds, :accession_id, foreign_key: true
-    add_reference :plant_samples, :accession_id, foreign_key: true
-    remove_index :seeds, name: "uniqueness_index"
-    add_index :seeds, %i[species generation accession_id], name: 'uniqueness_index', unique: true
     remove_column :seeds, :accession
     remove_column :seeds, :population_id
     remove_column :plant_samples, :population_id
+    remove_index :seeds, name: "uniqueness_index"
+
+    add_reference :seeds, :accession, foreign_key: true
+    add_reference :plant_samples, :accession, foreign_key: true
+    add_index :seeds, %i[species generation accession_id], name: 'uniqueness_index', unique: true
   end
 end
