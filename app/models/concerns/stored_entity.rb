2c47c003fa7a31e8bddcb7b4dc026f7e64bea694
kamalasaurus <kamalasaurus@gmail.com>
2022-10-05 14:01:39 -0400

    starting to ingest minus-80 samples

---
+++ app/models/concerns/stored_entity.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class  StoredEntity < ApplicationRecord
+
+end
--- app/services/plant_sample_upload.rb
+++ app/services/plant_sample_upload.rb
@@ -88,12 +88,82 @@ module PlantSampleUpload
   def self.parse_minus_80(file)
     col_sep = (file =~ /\t/ ? "\t" : ",")
     CSV.parse(file, headers: true, header_converters: %i[downcase symbol], col_sep: col_sep) do |row|
+      h = row.to_h
+
+      label = h
+        .slice(*%i[
+          accession
+          dedicated_box
+          dedicated_position
+        ])
+        .values.map(&:to_s)
+        .join('-')
+
+      attrs = h
+        .reject { |k, v| k == :discarded && v == 'yes' }
+        .slice(*%i[
+          tray_id
+          tray_rep
+          row
+          column
+          nb_samples
+        ])
+        .transform_keys({
+          tray_id: :accession_tray,
+          tray_rep: :replication_tray,
+          nb_samples: :quantity
+        })
+        .merge({
+          species: 'draba-verna',
+          label: label,
+          storage_method: 'minus-80'
+        })
+      
+      name, subpopulation, accession = h[:accession].split('-')
+      accession = accession.rjust(2, '0')
+
+      ActiveRecord::Base.transaction do
+        population_id = Population.find_by(name: name, subpopulation: subpopulation).id
+        full_attrs = attrs.merge({population_id: population_id})
+        PlantSample.upsert(full_attrs)
+
+        # the full_attrs do not guarantee uniqueness
+        # this call to last would be unsafe in multi-threaded ingestion
+        # but since single-threaded, it's probably fine for this scale
+        plant_sample_id = PlantSample.where(full_attrs).last.id
+
+        seed_attrs = {
+          species: attrs[:species],
+          population_id: population_id,
+          accession: accession
+        }
+
+        seeds = Seed.where(seed_attrs)
+        seeds.each do |seed|
+            SeedsPlantSample.upsert({
+              seed_id: seed.id,
+              plant_sample_id: plant_sample_id
+            })
+          end
+        if seeds.length == 0
+          puts 'NO SEEDS'
+          puts SeedsPlantSample.where(plant_sample_id: plant_sample_id).map(&:id).join(' ')
+          puts plant_sample_id
+        end
+      rescue StandardError => e
+        puts e
+        puts seed_attrs
+        puts attrs
+      end
+    end
     end
   end
 
   def self.parse_silica(file)
     col_sep = (file =~ /\t/ ? "\t" : ",")
     CSV.parse(file, headers: true, header_converters: %i[downcase symbol], col_sep: col_sep) do |row|
+      h = row.to_h
+
     end
   end
 end
