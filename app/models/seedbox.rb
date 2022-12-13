2ad90c9907b3f6050df9cae2298531b5405469ba
kamalasaurus <kamalasaurus@gmail.com>
2022-12-13 16:53:03 -0500

    removed n+1 query

--- app/models/seedbox.rb
+++ app/models/seedbox.rb
@@ -9,11 +9,6 @@ class Seedbox < ApplicationRecord
         box[tube.position] = tube
       end
     end
-    # all.map.with_object({}) do |seedbox, hsh|
-    #   hsh[seedbox] = seedbox.tubes.map.with_object({}) do |tube, box|
-    #     box[tube.position] = tube
-    #   end
-    # end
     .sort_by { |seedbox, _| seedbox.name.to_i }
     .to_h
   end
