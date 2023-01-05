327c42ade2d1be90374daebfa410800aa2d1ff50
kamalasaurus <kamalasaurus@gmail.com>
2023-01-05 17:03:56 -0500

    the javascript wiring works for csv submission

---
+++ app/javascript/graphql-to-csv.js
@@ -0,0 +1,7 @@
+export default function graphqlToCsvButton() {
+  console.log("#graphiql-container > div > div > div.editorBar > div.resultWrap > section > div > div.CodeMirror-scroll > div.CodeMirror-sizer > div > div > div")
+}
+
+void function() {
+  window.graphqlToCsvButton = graphqlToCsvButton
+}()
