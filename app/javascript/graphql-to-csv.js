06de98130470bdfc9e0631998eb45ed04b14e6d8
kamalasaurus <kamalasaurus@gmail.com>
2023-01-06 16:36:43 -0500

    selector for iframe content

--- app/javascript/graphql-to-csv.js
+++ app/javascript/graphql-to-csv.js
@@ -1,5 +1,19 @@
 export default function graphqlToCsvButton() {
-  console.log("#graphiql-container > div > div > div.editorBar > div.resultWrap > section > div > div.CodeMirror-scroll > div.CodeMirror-sizer > div > div > div")
+  const output = document.querySelector('iframe')
+    .contentWindow.document.body
+    .querySelectorAll('#graphiql-container .CodeMirror-lines')
+  
+  const response = JSON.parse(
+    Array.from(output)
+      .map(e => e.textContent.replace(/\s+/g, '').replace(/xxxxxxxxxx/, '')).pop()
+  )
+  // const responseNode = document.querySelector("#graphiql-container > div > div > div.editorBar > div.resultWrap > section > div > div.CodeMirror-scroll > div.CodeMirror-sizer > div > div > div")
+  // const responseString = JSON.parse(
+  //   Array.from(response.children)
+  //     .filter(el => el.style.display !== 'none')
+  //     .map(e => e.textContent.replace(/\s+/g, ''))
+  //     .join('')
+  // )
 }
 
 void function() {
