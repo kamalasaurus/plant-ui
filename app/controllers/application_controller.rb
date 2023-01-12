676b481fe838b5e3174c94c9d417cfe46ab73667
kamalasaurus <kamalasaurus@gmail.com>
2023-01-12 13:51:26 -0500

    added http basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV.fetch('PRODUCTION_DB_USERNAME'), \
+    password: ENV.fetch('PLANT_UI_DATABASE_PASSWORD')
 end
