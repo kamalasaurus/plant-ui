504ebe771570bbac091a9ca10fb2a9cde0319a96
kamalasaurus <kamalasaurus@gmail.com>
2023-02-07 17:12:27 -0500

    continuing

--- app/graphql/types/location_type.rb
+++ app/graphql/types/location_type.rb
@@ -30,6 +30,6 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
-    field :population, resolver: PopulationResolver
+    field :population, Types::PopulationType, null: false
   end
 end
