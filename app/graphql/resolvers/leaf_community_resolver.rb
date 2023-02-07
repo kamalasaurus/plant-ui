a316ac1611b38817b2829e5ded651ca99be66437
kamalasaurus <kamalasaurus@gmail.com>
2023-02-07 15:22:49 -0500

    leaf community resolver

---
+++ app/graphql/resolvers/leaf_community_resolver.rb
@@ -0,0 +1,29 @@
+class LeafCommunityResolver < BaseResolver
+  type [Types::LeafCommunityType], null: false
+
+  scope { LeafCommunity.all }
+
+  option(:population_id, type: Integer, description: 'select by population id') do |scope, value|
+    scope.where(population_id: value)
+  end
+
+  option(:population_ids, type: [Integer], description: 'select by multiple population ids') do |scope, value|
+    scope.where(population_id: value)
+  end
+
+  option(:population_name, type: String, description: 'select by population name') do |scope, value|
+    scope.includes(:population).select { |leaf| leaf.population.name == value.upcase }
+  end
+
+  option(:population_names, type: [String], description: 'select by multiple population names') do |scope, value|
+    scope.includes(:population).select { |leaf| value.map(&:upcase).includes? leaf.population.name }
+  end
+
+  option(:otu, type: String, description: 'select by an otu') do |scope, value|
+    scope.select { |leaf| leaf.otus.include? value.downcase }
+  end
+
+  option(:otus, type: [String], description: 'select by a list of otus') do |scope, value|
+    scope.select { |leaf| !(leaf.otus & value.map(&:downcase)).empty? } # single ampersand intersects arrays in ruby!
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -33,30 +33,19 @@ module Types
       end
     end
 
-    field :leaf_communities, [Types::LeafCommunityType], null: false,
-                                                         description: 'Return a list of leaf communities' do
-      argument :id, ID, required: false
-    end
-    def leaf_communities(*args)
-      if args.dig(0, :id)
-        [LeafCommunity.find(args.dig(0, :id))]
-      else
-        LeafCommunity.all
-      end
-    end
-
-    # field :locations, [Types::LocationType], null: false,
-    #                                          description: 'Return a list of locations' do
+    # field :leaf_communities, [Types::LeafCommunityType], null: false,
+    #                                                      description: 'Return a list of leaf communities' do
     #   argument :id, ID, required: false
     # end
-    # def locations(*args)
+    # def leaf_communities(*args)
     #   if args.dig(0, :id)
-    #     [Location.find(args.dig(0, :id))]
+    #     [LeafCommunity.find(args.dig(0, :id))]
     #   else
-    #     Location.all
+    #     LeafCommunity.all
     #   end
     # end
 
+    field :leaf_communities, resolver: LeafCommunityResolver, description: 'Return a list of leaf communities'
     field :locations, resolver: LocationResolver, description: 'Return a list of locations'
     field :plant_neighborhoods, resolver: PlantNeighborhoodResolver, description: 'Return a list of plant neighborhoods'
     field :plant_samples, resolver: PlantSampleResolver, description: 'Return a list of plant samples'
