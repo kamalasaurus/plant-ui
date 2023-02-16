528651fdec48cb9502dc6bddee5c1219a5cbb3f6
kamalasaurus <kamalasaurus@gmail.com>
2023-02-16 18:57:38 -0500

    mapped the bacterial species to parseable format

--- db/migrate/20230215212150_add_fields_to_species_and_bacteria_accessions.rb
+++ db/migrate/20230215212150_add_fields_to_species_and_bacteria_accessions.rb
@@ -1,6 +1,14 @@
 class AddFieldsToSpeciesAndBacteriaAccessions < ActiveRecord::Migration[7.0]
   def change
     add_column :species, :common_name, :string
+    add_column :species, :kingdom, :string
     add_column :bacteria_accessions, :uncertain_species, :boolean
+
+    create_table :subspecies_or_strain do |t|
+      t.string :strain
+      t.references :species, null: false, foreign_key: true
+
+      t.timestamps
+    end
   end
 end
