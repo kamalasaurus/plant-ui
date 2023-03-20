b4511989142c65c93dad8fbb798f98224f6c3d28
kamalasaurus <kamalasaurus@gmail.com>
2023-03-20 17:04:04 -0400

    exposed mutation types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -14,5 +14,13 @@ module Types
     field :create_seed, mutation: Mutations::CreateSeed, description: 'create a seed entry'
     field :create_plant_sample, mutation: Mutations::CreatePlantSample, description: 'create a plant sample entry'
     field :create_tube, mutation: Mutations::CreateTube, description: 'create a tube entry'
+    # begin additional bacteria types:
+    field :create_freezer, mutation: Mutations::CreateFreezer, description: 'create a freezer entry'
+    field :create_freezer_rack, mutation: Mutations::CreateFreezerRack, description: 'create a freezer rack entry'
+    field :create_bacteria_box, mutation: Mutations::CreateBacteriaBox, description: 'create a bacteria box entry'
+    field :create_bacteria_population, mutation: Mutations::CreateBacteriaPopulation, description: 'create a bacteria population entry'
+    field :create_bacteria_location, mutation: Mutations::CreateBacteriaLocation, description: 'create a bacteria location entry'
+    field :create_bacteria_accession, mutation: Mutations::CreateBacteriaAccession, description: 'create a bacteria accession entry'
+    field :create_bacteria_tube, mutation: Mutations::CreateBacteriaTube, description: 'create a bacteria tube entry'
   end
 end
